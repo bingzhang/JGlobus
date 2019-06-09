2448cc60211bb4c5dae817b0598ecd60bbcfedd8
Bing Zhang <bing@illinois.edu>
2019-06-09 16:18:23 -0500

    add examples model

--- gsiftp.sh
+++ gsiftp.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/usr/bin/java -cp gridftp/target/gridftp-2.2.0-SNAPSHOT.jar:ssl-proxies/target/ssl-proxies-2.2.0-SNAPSHOT.jar:gss/target/gss-2.2.0-SNAPSHOT.jar:jsse/target/jsse-2.2.0-SNAPSHOT.jar:lib/* org.globus.ftp.examples.GSIFtp "$@"
+#/usr/bin/java -cp examples/target/examples-2.2.0-SNAPSHOT.jar:ssl-proxies/target/ssl-proxies-2.2.0-SNAPSHOT.jar:gss/target/gss-2.2.0-SNAPSHOT.jar:lib/* org.globus.examples.GSIFTP "$@"
+
+/usr/bin/java -cp examples/target/examples-2.2.0-SNAPSHOT.jar org.globus.examples.GSIFTP "$@"
